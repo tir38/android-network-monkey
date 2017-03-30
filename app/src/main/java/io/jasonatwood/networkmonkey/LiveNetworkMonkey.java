7aa34c1dc7ca5d17cc6b112d615fc36191d81736
Jason Atwood <jason.atwood@stablekernel.com>
2017-03-30 14:07:10 -0400

    Initial commit

---
+++ app/src/main/java/io/jasonatwood/networkmonkey/LiveNetworkMonkey.java
@@ -0,0 +1,128 @@
+package io.jasonatwood.networkmonkey;
+
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Request;
+import okhttp3.Response;
+
+
+public class LiveNetworkMonkey implements NetworkMonkey {
+
+    private static final String TAG = "NETWORK MONKEY";
+    private final Context applicationContext;
+
+    private boolean shouldMonkeyWithWifiConnection;
+    private boolean shouldMonkeyWithResponseCode;
+    private boolean shouldMonkeyWithRequestSuccess;
+    private boolean jerkMode;
+    private int delayInMilliseconds;
+
+    public LiveNetworkMonkey(Context context) {
+        applicationContext = context.getApplicationContext();
+    }
+
+    @Override
+    public void shouldMonkeyWithWifiConnection() {
+        shouldMonkeyWithWifiConnection = true;
+    }
+
+    @Override
+    public void shouldMonkeyWithResponseCode() {
+        shouldMonkeyWithResponseCode = true;
+    }
+
+    @Override
+    public void shouldMonkeyWithResponseTime(int delayInMilliseconds) {
+        this.delayInMilliseconds = delayInMilliseconds;
+    }
+
+    @Override
+    public void shouldMonkeyWithRequestSuccess() {
+        shouldMonkeyWithRequestSuccess = true;
+    }
+
+    @Override
+    public void enableJerkMode() {
+        jerkMode = true;
+    }
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Request request = chain.request();
+        disableWifi();
+        addResponseDelay();
+        simulateRequestFailure();
+        Response response = chain.proceed(request);
+        return setResponseCodeTo404(response);
+    }
+
+    private void disableWifi() {
+        if (!shouldMonkeyWithWifiConnection || !shouldRandomlyDoSomething()) {
+            return;
+        }
+
+        WifiManager wifi = (WifiManager) applicationContext
+                .getApplicationContext()
+                .getSystemService(Context.WIFI_SERVICE);
+
+        wifi.setWifiEnabled(false);
+        Log.e(TAG, "Turning off wifi.");
+
+        // sleep so that wifi manager has time to turn off wifi antenna
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // TODO turn wifi back on
+    }
+
+    private Response setResponseCodeTo404(Response response) {
+        if (!shouldMonkeyWithResponseCode || !shouldRandomlyDoSomething()) {
+            return response;
+        }
+
+        // if something really did go wrong, we want to know about it.
+        if (!response.isSuccessful()) {
+            return response;
+        }
+
+        Log.e(TAG, "Changing response code to 404.");
+        return response.newBuilder()
+                .code(404)
+                .build();
+    }
+
+    private void addResponseDelay() {
+        if (delayInMilliseconds == 0 || !shouldRandomlyDoSomething()) {
+            return;
+        }
+
+        Log.e(TAG, "Delaying response by " + delayInMilliseconds + " milliseconds");
+        try {
+            Thread.sleep(delayInMilliseconds);
+        } catch (InterruptedException e) {
+            // eat it
+        }
+    }
+
+    private void simulateRequestFailure() {
+        if (shouldMonkeyWithRequestSuccess && shouldRandomlyDoSomething()) {
+            Log.e(TAG, "Simulating request failure");
+            throw new RuntimeException("Monkey Exception");
+        }
+    }
+
+    private boolean shouldRandomlyDoSomething() {
+        if (jerkMode) {
+            return (System.currentTimeMillis() % 2 == 0); // 1:2 chance of doing something
+        }
+
+        return (System.currentTimeMillis() % 10 == 0); // 1:10 chance of doing something
+    }
+}
---
+++ app/src/main/java/io/jasonatwood/networkmonkey/NetworkMonkey.java
@@ -0,0 +1,36 @@
+package io.jasonatwood.networkmonkey;
+
+import okhttp3.Interceptor;
+
+/**
+ * An OkHttp {@link Interceptor} that intercepts requests and responses
+ * and monkeys around with them.
+ */
+public interface NetworkMonkey extends Interceptor {
+
+    /**
+     * Tell NetworkMonkey to randomly turn off wifi connection
+     */
+    void shouldMonkeyWithWifiConnection();
+
+    /**
+     * Tell NetworkMonkey to randomly change response code to 404.
+     */
+    void shouldMonkeyWithResponseCode();
+
+    /**
+     * Tell NetworkMonkey to randomly delay responses
+     * @param delayInMilliseconds time to delay in milliseconds
+     */
+    void shouldMonkeyWithResponseTime(int delayInMilliseconds);
+
+    /**
+     * Tell NetworkMonkey to randomly fail network request
+     */
+    void shouldMonkeyWithRequestSuccess();
+
+    /**
+     * Tell NetworkMonkey to do all the things more often
+     */
+    void enableJerkMode();
+}
